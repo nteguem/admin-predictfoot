438f764a9305a88059e8364cae07da06ff396684
Cedric <c.mouafo-ext@fred.fr>
2022-12-17 14:05:29 +0100

    revie

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const path = require('path');
+var cors = require('cors');
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(cors())
 
 // Start the app predictfoot
 app.listen(process.env.PORT || 8000, function() {
