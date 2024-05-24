ca40aaf3d90d5946543b85f9b60e65a9dbda327c
nteguem roland <nteguemroland@gmail.com>
2024-05-24 17:47:24 +0100

    review

---
+++ makeda-dash.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path');
+var cors = require('cors');
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(cors())
+
+// Start the app predictfoot
+app.listen(process.env.PORT || 8000, function() {
+    console.log("app makeda ussd live  started")
+})
